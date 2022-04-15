2f47d993df95862f58c90845e864e6c02bc6ecfc
Yaguaret3 <n.leyes.n@gmail.com>
2022-04-14 21:04:30 -0300

    2 -Implementing Spring Security Usuario Autenticado. A continuación autorizaciones.

--- src/main/java/com/alkemy/springboot_challenge/configuration/SecurityConfig.java
+++ src/main/java/com/alkemy/springboot_challenge/configuration/SecurityConfig.java
@@ -1,26 +1,34 @@
 package com.alkemy.springboot_challenge.configuration;
 
+import com.alkemy.springboot_challenge.services.ChallengeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    ChallengeUserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/characters").hasAuthority("USER")
                 .antMatchers("/").permitAll();
+    }*/
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
